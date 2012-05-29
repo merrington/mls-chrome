97f430e187177346ff2b63ff9280690a0218cc93
Mike Errington <mike.errington@gmail.com>
2012-05-29 02:04:26 -0400

    Popup shows save/delete buttons only on property details page. Saved list shows number of saved properties

--- popup.js
+++ popup.js
@@ -8,6 +8,11 @@ chrome.tabs.getSelected(null, function(currentTab) {
 });
 
 function setPid(url) {
+	if (url.indexOf("propertyDetails.aspx") == -1) {
+		//this is not a property details page, hide the save/delete buttons!
+		$("#saveBtn").remove();
+		$("#deleteBtn").remove();
+	}
 	var pidStart = url.indexOf("=")+1;
 	var pidStop = url.indexOf("&");
 	pid = url.slice(pidStart, pidStop);
